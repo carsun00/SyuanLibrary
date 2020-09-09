44b990c8052b927618aa617bd538b729133828f3
SyuanTsai <carsun00@Gmail.com>
2020-09-09 11:01:44 +0800

    Entity Aes Crypto and Algorithm. 實體化Crypto, Algorithm.

---
+++ Crypto/Controller/Crypto/AesCrypto.cs
@@ -0,0 +1,80 @@
+﻿using Crypto.Abstract.Crypto;
+using Crypto.Model.Crypto;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Crypto.Controller.Crypto
+{
+    public class AesCrypto : AbstractCrypto
+    {
+        #region 方法
+        /// <summary>
+        ///     解密
+        /// </summary>
+        /// <param name="toDecrypt"></param>
+        /// <returns>string</returns>
+        public override string DeCrypto(string toDecrypt)
+        {
+            try
+            {
+                ICryptoTransform cTransform = Crypto.CreateDecryptor();
+                byte[] enBytes = Convert.FromBase64String(toDecrypt);
+                byte[] resultArray = cTransform.TransformFinalBlock(enBytes, 0, enBytes.Length);
+                Crypto.Clear();
+
+                return Encoding.UTF8.GetString(resultArray);
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                if(Crypto != null)
+                {
+                    Crypto.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        ///     加密
+        /// </summary>
+        /// <param name="toEncrypt"></param>
+        /// <returns>string</returns>
+        public override string EnCrypto(string toEncrypt)
+        {
+            try
+            {
+                byte[] toEncryptArray = Encoding.UTF8.GetBytes(toEncrypt);
+                byte[] resultArray = Crypto.CreateEncryptor().TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+                return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        /// <summary>
+        ///     初始化加密物件。
+        /// </summary>
+        /// <param name="CryptoParamater">0</param>
+        /// <returns></returns>
+        public override SymmetricAlgorithm GenerateCrypto(AesAlgorithm CryptoParamater)
+        {
+            Crypto = new AesCryptoServiceProvider
+            {
+                Key = CryptoParamater.Key,
+                IV = CryptoParamater.IV,
+                Mode = CryptoParamater.Mode,
+                Padding = CryptoParamater.Padding
+            };
+            return Crypto;
+        }
+
+        #endregion
+    }
+}
---
+++ Crypto/Model/Crypto/AesAlgorithm.cs
@@ -0,0 +1,22 @@
+﻿using Crypto.Abstract.Crypto;
+using System.Text;
+
+namespace Crypto.Model.Crypto
+{
+    /// <summary>
+    ///     會因各自程式不同需求，有不同的位元編碼方式
+    /// </summary>
+    public class AesAlgorithm : AbstractSymmetricAlgorithm
+    {
+        /// <summary>
+        ///     自定義的Encoding格式
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public override byte[] EncodingFormat(string key)
+        {
+            return Encoding.UTF8.GetBytes(key);
+        }
+
+    }
+}
