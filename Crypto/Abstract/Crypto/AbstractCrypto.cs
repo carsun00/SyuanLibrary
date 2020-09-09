19788a3f1babf887e148cb03e12ad4d1d4917367
SyuanTsai <carsun00@Gmail.com>
2020-09-09 10:58:41 +0800

    Add Abstract Crypto and Symmteric Algorithm class. 新增抽象Crypto與Symmteric Algorithm.

---
+++ Crypto/Abstract/Crypto/AbstractCrypto.cs
@@ -0,0 +1,89 @@
+﻿using Crypto.Model.Crypto;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace Crypto.Abstract.Crypto
+{
+    /// <summary>
+    ///     加密相關請以此方法作衍生。
+    /// </summary>
+    public abstract class AbstractCrypto : IDisposable
+    {
+        #region 宣告參數
+
+        #region Disposable相關
+
+        /// <summary>
+        ///     釋放非受控資源
+        /// </summary>
+        private bool disposed = false;
+
+        /// <summary>
+        ///     安全控制
+        /// </summary>
+        private readonly SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        #endregion
+
+        /// <summary>
+        ///     加密演算法
+        /// </summary>
+        public SymmetricAlgorithm Crypto { get; set; }
+
+        #endregion
+
+        #region 抽象方法
+
+        /// <summary>
+        ///     初始化加密物件，設定你要的加密類型。
+        ///     EX: AesCryptoServiceProvider
+        /// </summary>
+        /// <param name="CryptoParamater"></param>
+        /// <returns></returns>
+        public abstract SymmetricAlgorithm GenerateCrypto(AesAlgorithm CryptoParamater);
+
+        /// <summary>
+        ///     加密
+        /// </summary>
+        /// <param name="Crypto"></param>
+        /// <returns>回傳加密後的密文(string)</returns>
+        public abstract string EnCrypto(string toEncrypt);
+
+        /// <summary>
+        ///     解密
+        /// </summary>
+        /// <param name="Crypto"></param>
+        /// <returns>回傳解密後的明文(string)</returns>
+        public abstract string DeCrypto(string toDecrypt);
+
+        #endregion
+
+        #region Dispose 資源控制
+
+        /// <summary>
+        ///     使用完畢後呼叫方法，給予GC控制。
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///     實際釋放，交由程式控管。
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposed) return;
+            if(disposing)
+            {
+                handle.Dispose();
+            }
+            disposed = true;
+        }
+        #endregion
+    }
+}
---
+++ Crypto/Abstract/Crypto/AbstractSymmetricAlgorithm.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace Crypto.Abstract.Crypto
+{
+    /// <summary>
+    ///     金匙設定集中控管
+    /// </summary>
+    public abstract class AbstractSymmetricAlgorithm : IDisposable
+    {
+        #region 宣告參數
+
+        #region Disposable相關
+
+        /// <summary>
+        ///     釋放非受控資源
+        /// </summary>
+        private bool disposed;
+
+        /// <summary>
+        ///     安全控制
+        /// </summary>
+        private readonly SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        #endregion
+
+        /// <summary>
+        ///     加密的金匙
+        /// </summary>
+        protected internal byte[] Key;
+
+        /// <summary>
+        ///     加密偏移量
+        /// </summary>
+        protected internal byte[] IV;
+
+        /// <summary>
+        ///     編碼模式
+        /// </summary>
+        protected internal CipherMode Mode;
+
+        /// <summary>
+        ///     補零模式。
+        /// </summary>
+        protected internal PaddingMode Padding;
+
+        /// <summary>
+        ///     要加解密的訊息
+        /// </summary>
+        protected string CryptMsg;
+
+        #endregion
+
+        #region 實體方法
+
+        /// <summary>
+        ///     設定加密金匙
+        /// </summary>
+        /// <param name="key"></param>
+        public void SetKey(string key)
+        {
+            Key = EncodingFormat(key);
+        }
+
+        /// <summary>
+        ///     設定偏移金匙
+        /// </summary>
+        /// <param name="iv"></param>
+        public void SetIV(string iv)
+        {
+            IV = EncodingFormat(iv);
+        }
+
+        /// <summary>
+        ///     設定編碼格式
+        /// </summary>
+        /// <param name="iv"></param>
+        public void SetMode(CipherMode mode)
+        {
+            Mode = mode;
+        }
+
+        /// <summary>
+        ///     設定偏移金匙
+        /// </summary>
+        /// <param name="iv"></param>
+        public void SetPadding(PaddingMode padding)
+        {
+            Padding = padding;
+        }
+
+        #endregion
+
+        #region 抽象方法
+
+        /// <summary>
+        ///     指定自己的編碼格式
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public abstract byte[] EncodingFormat(string key);
+
+        #endregion
+
+        #region Dispose 資源控制
+
+        /// <summary>
+        ///     使用完畢後呼叫方法，給予GC控制。
+        /// </summary>
+        protected virtual void Dispose(bool disposing)
+        {
+            if(!disposed)
+            {
+                if(disposing)
+                {
+                    handle.Dispose();
+                }
+
+                disposed = true;
+            }
+        }
+
+        /// <summary>
+        ///     實際釋放，交由程式控管。
+        /// </summary>
+        /// <param name="disposing"></param>
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
