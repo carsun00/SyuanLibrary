cbc41369b63cd92df32b7d5a49f1b2165fdee5ed
SyuanTsai <carsun00@Gmail.com>
2020-09-09 15:32:32 +0800

    Add TripleDesc 新增 TripleDesc  實作

---
+++ Crypto/Model/Crypto/TripleDescAlgorithm.cs
@@ -0,0 +1,35 @@
+﻿using Crypto.Abstract.Crypto;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Crypto.Model.Crypto
+{
+    public class TripleDescAlgorithm : AbstractSymmetricAlgorithm
+    {
+        #region 覆寫的方法
+        /// <summary>
+        ///     自定義的Encoding格式
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public override byte[] EncodingFormat(string key)
+        {
+            return Encoding.UTF8.GetBytes(key);
+        }
+
+        /// <summary>
+        ///     TripleDesc 的Key需要特別編碼。
+        /// </summary>
+        /// <param name="key"></param>
+        public override void SetKey(string key)
+        {
+
+            MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+            byte[] keyBytes = hashmd5.ComputeHash(Encoding.UTF8.GetBytes(key));
+            hashmd5.Clear();
+            Key = keyBytes;
+        }
+        #endregion
+
+    }
+}
